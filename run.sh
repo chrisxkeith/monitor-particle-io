5685094b463211a0d256f3ef1ee879a9c92aec69
CK <chris@verdical.io>
2018-04-12 11:55:32 -0700

    First cut, does not work yet.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+set -x
+cd ~/Documents/Github/monitor-particle-io
+															if [ $? -ne 0 ] ; then exit -6 ; fi
+mvn clean install
+															if [ $? -ne 0 ] ; then exit -6 ; fi
+java -cp \
+	target\\build-particle-monitor-0.0.1-SNAPSHOT.jar \
+	io.verdical.monitor.UnitWatcher &
