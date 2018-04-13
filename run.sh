197e7135d9c10b1938b8269d486b54d2e157fc19
chrisxkeith <chris.keith@gmail.com>
2018-04-13 16:06:56 -0700

    jar builds with manifest, but still doesn't run.

--- run.sh
+++ run.sh
@@ -3,9 +3,13 @@ set -x
 
 cd ~/Documents/Github/monitor-particle-io						; if [ $? -ne 0 ] ; then exit -6 ; fi
 mvn clean install												; if [ $? -ne 0 ] ; then exit -6 ; fi
+ls -l target/build-particle-monitor-0.0.1-SNAPSHOT.jar			; if [ $? -ne 0 ] ; then exit -6 ; fi
+rm -f target/build-particle-monitor-0.0.1-SNAPSHOT.jar			; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 '/c/Program Files/Java/jdk1.8.0_65/bin/jar.exe'					\
 	-cvfm 'target\build-particle-monitor-0.0.1-SNAPSHOT.jar'	\
 	'src\main\resources\META-INF\MANIFEST.MF'					\
 	'target\classes\io\verdical\monitor\UnitWatcher.class'		; if [ $? -ne 0 ] ; then exit -6 ; fi
-java -jar target\\build-particle-monitor-0.0.1-SNAPSHOT.jar		; if [ $? -ne 0 ] ; then exit -6 ; fi
+
+ls -l target/build-particle-monitor-0.0.1-SNAPSHOT.jar			; if [ $? -ne 0 ] ; then exit -6 ; fi
+java -jar 'target\build-particle-monitor-0.0.1-SNAPSHOT.jar'	; if [ $? -ne 0 ] ; then exit -6 ; fi
