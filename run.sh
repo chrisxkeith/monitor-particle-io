9fa0bc7a43584d7de927d96cee08a67565325fcd
chrisxkeith <chris.keith@gmail.com>
2018-04-14 12:41:34 -0700

    Finally runs. Gets another error.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -x
 pushd ~/Documents/Github/monitor-particle-io					; if [ $? -ne 0 ] ; then exit -6 ; fi
 mvn clean install												; if [ $? -ne 0 ] ; then exit -6 ; fi
 java -cp \
-	'target\build-particle-monitor-0.0.1-SNAPSHOT.jar'			\
-	io.verdical.monitor.UnitWatcher &
+	'target\build-particle-monitor-0.0.1-SNAPSHOT.jar;\Users\Chris\.m2\repository\org\seleniumhq\selenium\selenium-api\3.11.0\*' \
+	io.verdical.monitor.UnitWatcher
 popd
